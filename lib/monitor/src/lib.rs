ae752e7cb6851957732afac3297c116725dd2c40
nabbisen <nabbisen@scqr.net>
2025-12-09 06:56:53 +0900

    check routines loop

--- lib/monitor/src/lib.rs
+++ lib/monitor/src/lib.rs
@@ -1,3 +1,23 @@
+use std::{
+    thread::sleep,
+    time::{Duration, SystemTime},
+};
+
+const CHECKS: [&str; 3] = ["http", "https cert", "email"];
+const INTERVAL: Duration = Duration::from_secs(10);
+
 pub fn run() -> () {
-    println!("Hello from monitor.");
+    let start = SystemTime::now();
+
+    loop {
+        for check in CHECKS {
+            println!(
+                "[{:04} secs] Hello from monitor. {}",
+                SystemTime::now().duration_since(start).unwrap().as_secs(),
+                check,
+            );
+        }
+
+        sleep(INTERVAL);
+    }
 }
