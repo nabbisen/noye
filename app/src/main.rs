9495083639594ec8f0ff18c9feb31ce369f18909
nabbisen <nabbisen@scqr.net>
2025-12-09 06:43:37 +0900

    procs hello

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    let _ = noye_common::config();
+
+    std::thread::spawn(|| {
+        let _ = noye_monitor::run();
+    });
+
+    std::thread::spawn(|| {
+        let _ = noye_web::run();
+    });
+
+    loop {
+        std::thread::park();
+    }
 }
