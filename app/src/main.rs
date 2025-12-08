d48beb8f2d111a23fbf3a87a42749d26e9ceee45
nabbisen <nabbisen@scqr.net>
2025-12-09 07:25:07 +0900

    async concurrency; async app entry; domains loop

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,8 +1,9 @@
-fn main() {
+#[tokio::main]
+async fn main() {
     let _ = noye_common::config();
 
-    std::thread::spawn(|| {
-        let _ = noye_monitor::run();
+    tokio::spawn(async {
+        let _ = noye_monitor::run().await;
     });
 
     std::thread::spawn(|| {
--- lib/monitor/src/lib.rs
+++ lib/monitor/src/lib.rs
@@ -1,28 +1,49 @@
 use std::{
+    sync::Arc,
     thread::sleep,
     time::{Duration, SystemTime},
 };
 
+use tokio::sync::Semaphore;
+
+const DOMAINS: [&str; 2] = ["dom1", "dom2"];
 const CHECKS: [&str; 3] = ["http", "https cert", "email"];
 const INTERVAL: Duration = Duration::from_secs(10);
+const MAX_CONCURRENCY: usize = 4;
 
-pub fn run() -> () {
+pub async fn run() -> () {
+    let semaphore = Arc::new(Semaphore::new(MAX_CONCURRENCY));
+    let mut handles = Vec::new();
+
     let start = SystemTime::now();
 
     loop {
-        for check in CHECKS {
-            let rt = tokio::runtime::Runtime::new().unwrap();
-            rt.block_on(hello(start.clone(), check));
+        for domain in DOMAINS {
+            for check in CHECKS {
+                let semaphore = semaphore.clone();
+
+                let handle = tokio::spawn(async move {
+                    let _permit = semaphore.acquire().await.unwrap();
+                    hello(start.clone(), domain, check).await;
+                });
+
+                handles.push(handle);
+            }
+        }
+
+        for h in &handles {
+            let _ = h;
         }
 
         sleep(INTERVAL);
     }
 }
 
-async fn hello(start: SystemTime, check: &str) {
+async fn hello(start: SystemTime, domain: &str, check: &str) {
     println!(
-        "[{:04} secs] Hello from monitor. {}",
+        "[{:04} secs] Hello from monitor. {} {}",
         SystemTime::now().duration_since(start).unwrap().as_secs(),
+        domain,
         check,
     );
 }
